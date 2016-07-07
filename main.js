ba9983c13757e574d61e8faacc176982dff1d653
DJBillieVacances <bhg3110@yahoo.de>
2016-07-07 14:08:12 -0400

    Functional
    User cannot set the time, no breaks, etc. This is just the first version

--- main.js
+++ main.js
@@ -1,5 +1,52 @@
-var currentTime;
+var deadline;
+var increment = 25; //in minutes
+var remainder = {};
 
-currentTime = Date.now();
+var clock;
+var minutesSpan;
+var secondsSpan;
 
-console.log(currentTime);
+// design inspiration: egg timer, codepen, pomodoro clock
+
+function setDeadLine(currentTime) {
+	increment *= 60 * 1000;
+	deadline = new Date(Date.parse(currentTime) + increment);
+	getTimeRemaining(deadline, currentTime);
+}
+
+function getTimeRemaining(deadline) {
+	var t = deadline - Date.parse(new Date());
+	var seconds = Math.floor( (t/1000) % 60 );
+	var minutes = Math.floor( (t/1000/60) % 60 );
+	return {
+		'total': t,
+		'minutes': minutes,
+		'seconds': seconds
+	}
+}
+
+function initializeClock() {
+	var d = Date.now();
+	var currentTime = new Date(d);
+	setDeadLine(currentTime); // defines the deadline
+
+	function updateClock() {
+		var t = getTimeRemaining(deadline);
+		minutesSpan.innerHTML = t.minutes;
+		secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+		if(t.total < 1000) {
+			clearInterval(timeinterval);
+		}		
+	}
+
+	updateClock();
+	var timeinterval = setInterval(updateClock, 1000);
+}
+
+window.onload = function() {
+	clock = document.getElementById("clockdiv");
+	minutesSpan = clock.querySelector('.minutes');
+	secondsSpan = clock.querySelector('.seconds');
+
+	initializeClock();
+}
